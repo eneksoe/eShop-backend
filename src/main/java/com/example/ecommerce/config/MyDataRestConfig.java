4bcb491255db56ff1996a3f496ff468fd831635a
enekoe <enekose@gmail.com>
2022-09-25 13:30:43 +0300

    added MyDataRestConfig

---
+++ src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.config;
+
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.model.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedMethod = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethod))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethod));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethod))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethod));
+    }
+}
